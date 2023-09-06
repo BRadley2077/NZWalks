d63b1ffeed1b53983df315837aac39ccab99d401
bradl <bradl@BOOKOFSPELLS>
2023-09-06 12:59:05 -0400

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Brad", "Luke", "Ellie", "Katherine" };
+
+            return Ok(studentNames);
+        }
+
+    }
+}
